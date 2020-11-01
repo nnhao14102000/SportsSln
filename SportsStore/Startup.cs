cbab7e9333f9dce7bf25d252751bda4e3061ae3a
KelvinHao <nnhao9a3@gmail.com>
2020-11-01 21:22:46 +0700

    Add Navigation bar.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -30,6 +30,7 @@ namespace SportsStore
                 Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,14 +40,26 @@ namespace SportsStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseRouting();
-            
+
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("catpage",
+                "{category}/Page{productPage:int}",
+                new { Controller = "Home", action = "Index" });
+
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
+
+                endpoints.MapControllerRoute("category", "{category}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
+
                 endpoints.MapControllerRoute("pagination",
                 "Products/Page{productPage}",
-                new { Controller = "Home", action = "Index" });
+                new { Controller = "Home", action = "Index", productPage = 1 });
+                
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
             SeedData.EnsurePopulated(app);
         }
