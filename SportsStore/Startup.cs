7f9d9033f771ea7d8fabe3bdad5ee959beaf9457
KelvinHao <nnhao9a3@gmail.com>
2020-11-02 09:33:12 +0700

    Add Administrator with Blazor sever and add CRUD feature in addminstrator page

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -37,6 +37,8 @@ namespace SportsStore
             ///***********************************/
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //-------------------------------------------------------------------------
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +69,9 @@ namespace SportsStore
                 
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
+                //-----------------------------------------------------------------------
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
             SeedData.EnsurePopulated(app);
         }
